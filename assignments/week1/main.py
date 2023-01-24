592e167f997c84b1c47630342276e63594c415ed
senapathys <47801356+senapathys@users.noreply.github.com>
2023-01-24 15:57:33 -0500

    revert main

--- assignments/week1/main.py
+++ assignments/week1/main.py
@@ -1,6 +1,5 @@
 # Import your model:
 from model import LinearRegression
-from model import GradientDescentLinearRegression
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -47,7 +46,7 @@ def main():
     X_train, X_test, y_train, y_test = get_housing_data()
 
     # Create a linear regression model:
-    lr = GradientDescentLinearRegression()
+    lr = LinearRegression()
 
     # Fit the model to the training data:
     lr.fit(X_train, y_train)
