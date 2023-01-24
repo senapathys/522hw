4eba224bfae0231be54c5ef97a7ef15c02030664
senapathys <47801356+senapathys@users.noreply.github.com>
2023-01-24 13:43:52 -0500

    models

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -6,18 +6,17 @@ class LinearRegression:
     b: float
 
     def __init__(self):
-        self.coef_ = None
-        self.intercept_ = None
+        self.weights = None
+        self.bias = 0
 
     def fit(self, X: np.ndarray, y: np.ndarray):
-        X = np.hstack((np.ones((X.shape[0], 1)), X))  # add bias column
-        self.coef_ = np.linalg.inv(X.T @ X) @ X.T @ y
-        self.intercept_ = self.coef_[0]
-        self.coef_ = self.coef_[1:]
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        weights = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.weights = weights[1:]
+        self.bias = weights[0]
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        X = np.hstack((np.ones((X.shape[0], 1)), X))  # add bias column
-        return X @ np.append(self.intercept_, self.coef_)
+        return X @ self.weights + self.bias
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -26,20 +25,19 @@ class GradientDescentLinearRegression(LinearRegression):
     """
 
     def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000):
-        X = np.hstack((np.ones((X.shape[0], 1)), X))  # add bias column
-        n_samples, n_features = X.shape
-        self.coef_ = np.random.randn(n_features)
-        self.intercept_ = np.random.randn()
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        self.weights = np.zeros(X.shape[1]-1)
+        self.bias = 0
 
         for _ in range(epochs):
-            y_pred = X @ self.coef_ + self.intercept_
-            residuals = y - y_pred
-            gradient_coef = -(2 / n_samples) * X.T @ residuals
-            gradient_intercept = -(2 / n_samples) * np.sum(residuals)
-            print(y_pred)
-            self.coef_ -= lr * gradient_coef
-            self.intercept_ -= lr * gradient_intercept
+            print(self.weights)
+            print(self.bias)
+            y_pred = X @ np.hstack((self.bias, self.weights))
+            residuals = y_pred - y
+            gradient = X.T @ residuals
+            self.bias -= lr * gradient[0]
+            self.weights -= lr * gradient[1:]
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        X = np.hstack((np.ones((X.shape[0], 1)), X))  # add bias column
-        return X @ self.coef_ + self.intercept_
+        return X @ self.weights + self.bias
