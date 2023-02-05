d5173fbafd02662e2b834159662789bbabfbb935
senapathys <47801356+senapathys@users.noreply.github.com>
2023-02-05 11:06:52 -0500

    model 1

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,16 +1,21 @@
 import torch
 from typing import Callable
+import torch
+import torch.nn as nn
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """
+    Multi-layer perceptron class implementation
+    """
     def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_classes: int,
-        hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+            self,
+            input_size: int,
+            hidden_size: int,
+            num_classes: int,
+            hidden_count: int = 1,
+            activation: Callable = torch.nn.ReLU,
+            initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
         Initialize the MLP.
@@ -22,9 +27,27 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
 
-    def forward(self, x):
+        # Initialize the hidden layers
+        hidden_layers = []
+        for i in range(hidden_count):
+            hidden_layers.append(nn.Linear(input_size, hidden_size))
+            hidden_layers.append(activation())
+            input_size = hidden_size
+
+        # Initialize the output layer
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+
+        # Stack the hidden layers and output layer into the model
+        self.model = nn.Sequential(*hidden_layers, self.output_layer)
+
+        # Initialize the weights
+        for layer in self.model:
+            if hasattr(layer, "weight"):
+                initializer(layer.weight)
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +57,4 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        return self.model(x)
--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,4 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 32, output_dim, 1, torch.nn.ReLU, torch.nn.init.ones_)
+    return MLP(input_dim, 16, output_dim, 2, torch.nn.ReLU, torch.nn.init.ones_)
