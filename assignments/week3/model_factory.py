fa589b6d540d010fc2265d5e4682c4799f769587
senapathys <47801356+senapathys@users.noreply.github.com>
2023-02-05 12:55:40 -0500

    new model

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,5 +16,5 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     """
     return MLP(
-        input_dim, 128, output_dim, 1, torch.nn.ReLU, torch.nn.init.xavier_normal_
+        input_dim, 128, output_dim, 2, torch.nn.ReLU, torch.nn.init.xavier_normal_
     )
